f82050ec06663590f8f49f8d933274f4de9262fe
zhangshilong <adofsauron@163.com>
2023-08-03 15:36:16 +0800

    fix percona mysql monetdb

--- profill/profill_aggr.sql
+++ profill/profill_aggr.sql
@@ -2,8 +2,18 @@
 EXPLAIN format=tree
 select sum(d1) from d where d1 > 1 group by d1 \G
 
+EXPLAIN format=json
+select sum(d1) from d where d1 > 1 group by d1 \G
+
 
 EXPLAIN format=tree
+SELECT gender, AVG(score) AS avg_score
+FROM students
+WHERE age > 19
+GROUP BY gender
+HAVING AVG(score) > 90 \G
+
+EXPLAIN format=json
 SELECT gender, AVG(score) AS avg_score
 FROM students
 WHERE age > 19
--- profill/profill_leftjoin.sql
+++ profill/profill_leftjoin.sql
@@ -7,3 +7,11 @@ LEFT JOIN
 ) x
 ON b.b1 = x.c1 \G
 
+
+EXPLAIN format=json
+SELECT b.b1,x.c1 FROM b 
+LEFT JOIN 
+(
+    SELECT c1 FROM c INNER JOIN a ON a.a1 = c.c1 and a.a1 >= 1 and a.a1 < 4 and c.c1 >= 1 and c.c1 <= 3
+) x
+ON b.b1 = x.c1 \G
